439ab8b40c5ce72d57faf63669f9d56fd3e0ad80
Ivan Nekipelov <supervanyok@gmail.com>
2016-06-22 01:28:11 +0300

    Fix codestyle

--- bot.py
+++ bot.py
@@ -38,15 +38,12 @@ def send_statistic(message):
            "лета. Осталось проебать {}".format(days_spent, days_left)
     bot.send_message(message.chat.id, text)
 
-@bot.message_handler(func=lambda message: True, commands=['ban'])
-def send_statistic(message):
-    """BAN person for 5 minutes (or custom period of time)"""
-    pass
 
 @bot.message_handler(func=lambda message: True, commands=['tits'])
-def send_statistic(message):
+def send_tits(message):
     """Show me your kitis!"""
-    tits = ["https://s-media-cache-ak0.pinimg.com/236x/b5/74/b1/b574b154616e8d0c6d76efaa9de3a0a9.jpg",
+    tits = [
+        "https://s-media-cache-ak0.pinimg.com/236x/b5/74/b1/b574b154616e8d0c6d76efaa9de3a0a9.jpg",
         "https://s-media-cache-ak0.pinimg.com/236x/a2/2b/86/a22b863d140d6317e94232f5b9398845.jpg",
         "http://thumbs.bignudeboobs.com/th/2016-01-19/333779_02.jpg",
         "http://girl-tits.com/wp-content/uploads/2015/03/big-tits_-121.jpg",
@@ -54,8 +51,9 @@ def send_statistic(message):
         "https://pp.vk.me/c627530/v627530910/1a8f0/ksAsOAjXys0.jpg",
         "http://amateurinaction.com/wp-content/uploads/2012/04/photo-Babe-Big-Tits-Blonde-409014524.jpg",
         "http://hotfmodels.com/wp-content/uploads/2013/10/tqJBLD0.jpg",
-        "http://www.dirtyrottenwhore.com/wp-content/uploads/2015/03/Busty-Gorgeous-Shaved-Teen-Brunette-Babe-Yara-with-Perfect-Breasts-04.png",
-        "http://www.18porno.tv/contents/videos_screenshots/0/626/preview.mp4.jpg"]
+        "http://boobs-selfshots.tumblr.com/post/146134834279",
+        "http://www.18porno.tv/contents/videos_screenshots/0/626/preview.mp4.jpg"
+    ]
     rand = random.randint(0, len(tits) - 1)
     bot.send_message(message.chat.id, tits[rand])
 
